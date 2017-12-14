313edb7f5a8bf84b46f2f624c2b04fff075b8b55
Kedirech <brackcarmony@gmail.com>
2017-12-14 12:41:49 -0700

    Test.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const massive = require('massive');
 const app = express();
+
+massive(process.env.CONNECTION_STRING).then(db=>{
+    app.set('db', db);
+
+    db.new_planes().then(planes=>{console.log(planes)})
+        .catch(err=>console.error(err));
+}).catch(err=>console.error(err));
+
+
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
